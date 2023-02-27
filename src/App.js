bfa236dcf55a296238aad83ec239bc2069c3791d
poddui <jimi8888.podduikin@gmail.com>
2023-02-27 13:36:59 +0200

    ensimmäinen committi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Todotable from './components/Todolist'
 
 function App() {
+  const[todo, setTodo] = useState({desc: '', date: ''});
+  const[todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <label for="text">Tehtävä: </label>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <label for="date"> Päivämäärä: </label>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="submit" value="Add"/>
+      </form>
+      <Todotable todos={todos} />
     </div>
   );
 }
